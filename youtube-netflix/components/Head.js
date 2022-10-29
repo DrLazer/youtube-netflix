6d3d146731d56ee3682cf2d56cd8fd2d306a92ab
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2022-10-29 16:47:23 +0100

    setup sass

---
+++ youtube-netflix/components/Head.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NextHead from 'next/head'
+
+function Head() {
+  return (
+    <NextHead>
+      <title>Netflix United Kingdom – Watch TV Programmes Online, Watch Films Online</title>
+      <link rel="icon" href="/favicon.ico" />
+    </NextHead>
+  )
+}
+
+export default Head
---
+++ youtube-netflix/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Header.module.scss';
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      this is the header
+    </div>
+  )
+}
+
+export default Header
